9fc760b6c94be1438e66b0785a857bcff20eb1ba
kylesezhi <kyle.bedell@gmail.com>
2024-01-18 10:43:24 -0500

    Improve install button styling

--- src/containers/InstallButton/InstallButton.tsx
+++ src/containers/InstallButton/InstallButton.tsx
@@ -1,23 +1,35 @@
 import { useEffect, useState } from "react";
 import { useAddToHomescreenPrompt } from "../../hooks/useAddToHomescreenPrompt";
 import "./InstallButton.css";
+import installIcon from "./assets/install-icon.svg";
+import { useTarotStore } from "../../store";
 
 function InstallButton() {
   const [prompt, promptToInstall] = useAddToHomescreenPrompt();
   const [visible, setVisible] = useState<boolean>(false);
+  const isInterpretationShowing = useTarotStore(
+    (state) => state.isInterpretationShowing,
+  );
+  const isCardShowing = useTarotStore((state) => state.isCardShowing);
+  const show = visible && !isInterpretationShowing && !isCardShowing;
+
   useEffect(() => {
     if (prompt) {
       setVisible(true);
     }
   }, [prompt]);
+
   const onClick = async () => {
     if (prompt && typeof promptToInstall === "function") {
       await promptToInstall();
     }
   };
   return (
-    <div className={`install-button${visible ? "" : " hide"}`}>
-      <button onClick={onClick}>Install</button>
+    <div className={`install-button-container${show ? "" : " hide"}`}>
+      <button onClick={onClick} className="install-button">
+        <img src={installIcon} />
+        Install
+      </button>
     </div>
   );
 }
