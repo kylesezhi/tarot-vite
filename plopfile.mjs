29e4e35b0fc5ef4a4b557e3c5d2b32fce159e8ad
kylesezhi <kyle.bedell@gmail.com>
2024-01-17 19:38:25 -0500

    Improve plop script / templates

--- plopfile.mjs
+++ plopfile.mjs
@@ -1,4 +1,7 @@
 export default function (plop) {
+  plop.setHelper("kebab", (str) => {
+    return str.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
+  });
   plop.setGenerator("component", {
     description: "generate a component or container",
     prompts: [
@@ -12,6 +15,11 @@ export default function (plop) {
         type: "confirm",
         name: "hasPropInterface",
         message: "do you want a props interface?",
+      },
+      {
+        type: "confirm",
+        name: "hasStarterClass",
+        message: "do you want a starter class?",
       },
       {
         type: "input",
